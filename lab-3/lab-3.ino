b391fc4529a591d6e2fa2232a28ddfe027dcc2bd
Mark Anastos <MAnastos98@gmail.com>
2018-10-23 21:45:37 -0400

    got radio and gui working

--- lab-3/lab-3.ino
+++ lab-3/lab-3.ino
@@ -7,27 +7,25 @@
 #define LOG_OUT 1 // use the log output function
 #define FFT_N 256 // set to 256 point fft
 
-#include "RF24.h"
 #include <FFT.h> // include the library
 #include "Servo.h"
+#include "RF24.h"
 
+RF24 radio(9,10);
+
+const uint64_t pipes[2] = { 0x18LL, 0x19LL };
+
+int count = 0;
 volatile long l_timer, r_timer;
 volatile int l_reading, l_prev, r_reading, r_prev;
 volatile bool l_line, r_line;
-byte x = 0;
-byte y = 0;
-byte NESW = B0000;
-byte shape = B00;
-byte color = B0;
-byte orientation = 1; //nesw
-byte grid [9][9];
+
+byte x = 0, y = 0, orientation = 1;
+byte grid[9][9];
 
 int ADCSRA_initial, TIMSK0_initial;
 
 Servo servo_l, servo_r;
-
-// Set up nRF24L01 radio on SPI bus plus pins 9 & 10
-RF24 radio(9,10);
 
 void setup_sensor(int pin, volatile long *sensor_timer) {
   *sensor_timer = micros();
@@ -51,36 +49,11 @@ void r_isr() {
 }
 
 bool f_wall() {
-  return analogRead(WSENSOR_F_PIN) > 150;
-  
+  return grid[x][y] & (8 >> orientation);
 }
 
 bool r_wall() {
-  return analogRead(WSENSOR_R_PIN) > 150;
-}
-
-void check_walls() {
-  bool fwall = analogRead(WSENSOR_F_PIN) > 150;
-  bool rwall = analogRead(WSENSOR_R_PIN) > 150;
-  grid[x][y] |= 1 << 7;
-  switch (orientation) {
-    case 0:
-      if (fwall) grid[x][y] |= 8;
-      if (rwall) grid[x][y] |= 4;
-      break;
-    case 1:
-      if (fwall) grid[x][y] |= 4;
-      if (rwall) grid[x][y] |= 2;
-      break;
-    case 2:
-      if (fwall) grid[x][y] |= 2;
-      if (rwall) grid[x][y] |= 1;
-      break;
-    case 3:
-      if (fwall) grid[x][y] |= 1;
-      if (rwall) grid[x][y] |= 8;
-      break;
-  }
+  return grid[x][y] & (8 >> ((orientation + 1) % 4));
 }
 
 void l_forward() {
@@ -112,15 +85,7 @@ void forward() {
       r_forward();
       if (!l_line && !r_line) {
         delay(100);
-        if (orientation == 0)
-          y--;  
-        else if (orientation == 1)
-          x++;
-        else if (orientation == 2)
-          y++;
-        else if (orientation == 3)
-          x--;
-        return;
+        break;
       }
     } else if (r_line && l_line)
       at_intersection = true;
@@ -129,30 +94,26 @@ void forward() {
       r_line ? r_backward() : r_forward();
     }
   }
+  switch (orientation) {
+    case 0: y--; break;
+    case 1: x++; break;
+    case 2: y++; break;
+    case 3: x--; break;
+  }
 }
 
 void l_turn() {
   l_backward();
   r_forward();
-  if (orientation == 0){
-    orientation = 3;
-  }
-  else{
-    orientation--;
-  }
   delay(500);
+  orientation = (orientation - 1) % 4;
 }
 
 void r_turn() {
   l_forward();
   r_backward();
-  if (orientation == 3){
-    orientation = 0;
-  }
-  else{
-    orientation++;
-  }
   delay(500);
+  orientation = (orientation + 1) % 4;
 }
 
 void setup() {
@@ -163,9 +124,6 @@ void setup() {
   
   setup_sensor(LSENSOR_R_PIN, &r_timer);
   setup_sensor(LSENSOR_L_PIN, &l_timer);
-  
-  servo_l.attach(SERVO_L_PIN);
-  servo_r.attach(SERVO_R_PIN);
 
   ADCSRA_initial = ADCSRA;
   TIMSK0_initial = TIMSK0;
@@ -176,27 +134,23 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
   radio.begin();
-// optionally, increase the delay between retries & # of retries
   radio.setRetries(15,15);
   radio.setAutoAck(true);
-  // set the channel
   radio.setChannel(0x50);
-  // set the power
-  // RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_MED=-6dBM, and RF24_PA_HIGH=0dBm.
   radio.setPALevel(RF24_PA_MIN);
-  //RF24_250KBPS for 250kbs, RF24_1MBPS for 1Mbps, or RF24_2MBPS for 2Mbps
   radio.setDataRate(RF24_250KBPS);
-  radio.openWritingPipe(0x18LL);
 
-  delay(100);
-  servo_stop();
-  Serial.println("before tone");
-  int fft_prev = 0, fft_curr = 0;
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
+
+  radio.startListening();
+
   do {
     ADCSRA = 0xe7; // set the adc to free running mode
     TIMSK0 = 0; // turn off timer0 for lower jitter
     cli();  // UDRE interrupt slows this way down on arduino1.0
-    ADMUX = 0X41;
+
+    ADMUX = 0x41;
     for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
       while (!(ADCSRA & 0x10)); // wait for adc to be ready
       ADCSRA = 0xf7; // restart adc
@@ -215,12 +169,67 @@ void setup() {
     sei();
     ADCSRA = ADCSRA_initial;
     TIMSK0 = TIMSK0_initial;
-    fft_curr = fft_log_out[18]; 
-    Serial.println(fft_curr);
-    delay(500);
-  } while (fft_curr < 110);
-  Serial.println("after tone");
+  } while (fft_log_out[18] < 110);
 
+  servo_l.attach(SERVO_L_PIN);
+  servo_r.attach(SERVO_R_PIN);
+}
+
+void check_walls() {
+  bool fwall = analogRead(WSENSOR_F_PIN) > 150;
+  bool rwall = analogRead(WSENSOR_R_PIN) > 150;
+  grid[x][y] |= 1 << 7;
+  switch (orientation) {
+    case 0:
+      if (fwall) grid[x][y] |= 8;
+      if (rwall) grid[x][y] |= 4;
+      break;
+    case 1:
+      if (fwall) grid[x][y] |= 4;
+      if (rwall) grid[x][y] |= 2;
+      break;
+    case 2:
+      if (fwall) grid[x][y] |= 2;
+      if (rwall) grid[x][y] |= 1;
+      break;
+    case 3:
+      if (fwall) grid[x][y] |= 1;
+      if (rwall) grid[x][y] |= 8;
+      break;
+  }
+}
+
+void send_data() {
+  radio.stopListening();
+
+  byte data[] = {y, x, grid[x][y]};
+  Serial.println("Now sending " + String(data[0]));
+  bool ok = radio.write( &data, 3 * sizeof(byte) );
+
+  if (ok)
+    Serial.print("ok...");
+  else
+    Serial.println("failed.");
+
+  radio.startListening();
+
+  unsigned long started_waiting_at = millis();
+  bool timeout = false;
+  while ( ! radio.available() && ! timeout )
+    if (millis() - started_waiting_at > 200 )
+      timeout = true;
+
+  if ( timeout )
+  {
+    Serial.println("Failed, response timed out.");
+  }
+  else
+  {
+    byte got_data[3];
+    radio.read(&got_data, 3 * sizeof(byte));
+
+    Serial.println("Got response " + String(got_data[0]));
+  }
 }
 
 void check_robots() {
@@ -248,19 +257,14 @@ void check_robots() {
     sei();
     ADCSRA = ADCSRA_initial;
     TIMSK0 = TIMSK0_initial;
-    if (fft_log_out[42] > 70)
-      delay(500);
-  } while (fft_log_out[42] > 70);
+  } while (fft_log_out[42] > 100);
 }
 
 void loop() {
-  Serial.println("start loop");
-  check_walls();
-  Serial.println("before radio");
-  byte data[3] = {x, y, grid[x][y] };
-  while (!radio.write( &data, 3 * sizeof(byte) ));
-  Serial.println("after radio");
+  Serial.println(String(x) + ", " + String(y) + ", " + grid[x][y]);
   forward();
+  check_walls();
+  send_data();
   if (!r_wall()) {
     r_turn();
   } else if (f_wall()) {
--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,24 +1,46 @@
 #include "RF24.h"
 
-RF24 radio(9, 10);
+RF24 radio(9,10);
 
-void setup() {
+const uint64_t pipes[2] = { 0x18LL, 0x19LL };
+
+void setup(void) {
   Serial.begin(9600);
+
   radio.begin();
-  radio.setRetries(15, 15);
+  radio.setRetries(15,15);
   radio.setAutoAck(true);
   radio.setChannel(0x50);
   radio.setPALevel(RF24_PA_MIN);
   radio.setDataRate(RF24_250KBPS);
-  radio.openReadingPipe(1, 0x18LL);
+  radio.openWritingPipe(pipes[1]);
+  radio.openReadingPipe(1,pipes[0]);
+
   radio.startListening();
+
+  radio.printDetails();
 }
 
-void loop() {
-  if (radio.available()) {
+void loop(void)
+{
+  if ( radio.available() )
+  {
     byte data[3];
     bool done = false;
-    radio.read(&data, 3 * sizeof(byte));
+    while (!done)
+    {
+      done = radio.read( &data, 3 * sizeof(byte) );
+   //   Serial.println("Got payload " + String(data[0]));
+      delay(20);
+    }
+
+    radio.stopListening();
+
+    radio.write( &data, 3 * sizeof(byte) );
+   // Serial.println("Sent response.");
+
+    radio.startListening();
+
     String out = String(data[0]) + "," + String(data[1]);
     if (data[2] & 1)
       out += ",west=true";
@@ -42,6 +64,7 @@ void loop() {
     }
     if (shape)
       out += ",tcolor=" + ((data[2] & 16) ? String("red") : String("blue"));
+    out += ",iamhere=true";
     Serial.println(out);
   }
 }
